4921134c30ba3e502146ec41eeecd2b402eadd72
Saina <saina.h72@gmail.com>
2021-12-03 20:00:23 +0330

    Initial Commit

---
+++ RSS-Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSS-Reader
+//
+//  Created by MBAir on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
